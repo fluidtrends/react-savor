0862dc09036f793e4fd13def75139e9a05204314
I. Dan Calinescu <idancalinescu@gmail.com>
2020-01-29 02:00:13 +0200

    using nyc for coverage

--- bin/commands.js
+++ bin/commands.js
@@ -1,7 +1,7 @@
 var commands = require('savor/bin/commands')
 
 var coverageBin = "./node_modules/.bin/babel-node";
-var coverageLocalArgs = ['./node_modules/.bin/istanbul', 'cover', './node_modules/.bin/_mocha', '--', '--require', './node_modules/react-savor/res/includes.js', '--report', 'lcovonly', '--recursive', 'test/specs/**/*.js'];
+var coverageLocalArgs = ['./node_modules/.bin/nyc', '--reporter=lcov', 'npm', 'run', 'test'];
 var nativeCoverageLocalArgs = ['./node_modules/.bin/istanbul', 'cover', './node_modules/.bin/_mocha', '--', '--require', 'react-native-mock/mock', '--require', './node_modules/react-savor/res/includes.js', '--report', 'lcovonly', 'test/specs/**/*.js'];
 
 var testBin = "./node_modules/.bin/mocha"
