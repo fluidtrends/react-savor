d4a362f3c7514a16f6e6f5ac054b5138bcf68f1e
I. Dan Calinescu <idancalinescu@gmail.com>
2020-01-31 19:40:36 +0200

    using latest babel core

--- bin/commands.js
+++ bin/commands.js
@@ -1,11 +1,11 @@
 var commands = require('savor/bin/commands')
 
-var coverageBin = "./node_modules/.bin/babel-node";
-var coverageLocalArgs = ['./node_modules/.bin/nyc', '--reporter=lcov', 'npm', 'run', 'test'];
+var coverageBin = "./node_modules/.bin/nyc";
+var coverageLocalArgs = ['--reporter=lcov', 'npm', 'run', 'test'];
 var nativeCoverageLocalArgs = ['./node_modules/.bin/istanbul', 'cover', './node_modules/.bin/_mocha', '--', '--require', 'react-native-mock/mock', '--require', './node_modules/react-savor/res/includes.js', '--report', 'lcovonly', 'test/specs/**/*.js'];
 
 var testBin = "./node_modules/.bin/mocha"
-var testArgs = ['--require', 'babel-core/register', '--recursive', 'test/specs/**/*.js']
+var testArgs = ['--require', '@babel/register', '--recursive', 'test/specs/**/*.js']
 var nativeTestArgs = ['--require', 'react-native-mock/mock', '--require', './node_modules/react-savor/res/includes.js', '--require', 'babel-core/register', '--recursive', 'test/specs/**/*.js']
 
 commands.test = [{
