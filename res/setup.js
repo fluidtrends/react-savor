fc7a50ef5cd3a4e92d4c30f7460aa279ddcf15e5
I. Dan Calinescu <idancalinescu@gmail.com>
2020-02-03 09:46:38 +0200

    added window scrollto stub by default

--- res/setup.js
+++ res/setup.js
@@ -1,31 +1,30 @@
-const { JSDOM } = require('jsdom');
+const { JSDOM } = require('jsdom')
 
-const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
-const { window } = jsdom;
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>')
+const { window } = jsdom
 
 function copyProps(src, target) {
   Object.defineProperties(target, {
     ...Object.getOwnPropertyDescriptors(src),
     ...Object.getOwnPropertyDescriptors(target),
-  });
+  })
 }
 
-global.window = window;
-global.document = window.document;
-global.navigator = {
-  userAgent: 'node.js',
-};
-global.requestAnimationFrame = function (callback) {
-  return setTimeout(callback, 0);
-};
-global.cancelAnimationFrame = function (id) {
-  clearTimeout(id);
-};
+global.window = window
+global.window.scrollTo = () => ({})
+global.document = window.document
+
+global.navigator = { userAgent: 'node.js' }
+
+global.requestAnimationFrame = (callback) => setTimeout(callback, 0)
+global.cancelAnimationFrame = (id) => clearTimeout(id)
 global.fetch = () => Promise.resolve({ json: () => ({ data: "test" })})
+
 global.localStorage = {
   setItem: (key, value, callback) => callback(),
   getItem: (key, callback) => callback(null, JSON.stringify({ test: 'test', username: 'test' })),
   removeItem: (key, callback) => callback()
 }
 global.storage = global.localStorage
-copyProps(window, global);
+
+copyProps(window, global)
